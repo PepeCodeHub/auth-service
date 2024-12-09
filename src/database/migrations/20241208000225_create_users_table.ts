3aa6070bb4cc584e08541ae66aff58888fa7c36c
Ihor Cherniavskyi <chernyavskiy2019ios@gmail.com>
2024-12-09 21:34:34 +0200

    Refactor user table migration to use class-based structure with transaction handling and logging

--- src/database/migrations/20241208000225_create_users_table.ts
+++ src/database/migrations/20241208000225_create_users_table.ts
@@ -1,16 +1,49 @@
 import { Knex } from 'knex';
+import { MigrationLogger, migrationsLogger } from '../decorators';
 
-export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable('users', table => {
-    table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
-    table.string('email').unique().notNullable();
-    table.string('password').notNullable();
-    table.boolean('is_admin').defaultTo(false);
-    table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
-  });
+export class Migration {
+  @MigrationLogger
+  static async up(knex: Knex): Promise<void> {
+    await knex.transaction(async transaction => {
+      const isTableExists = await transaction.schema.hasTable('users');
+      if (isTableExists) {
+        migrationsLogger.warn('Table users already exists');
+        return;
+      }
+
+      await transaction.schema.createTable('users', table => {
+        table
+          .uuid('id')
+          .primary()
+          .defaultTo(transaction.raw('gen_random_uuid()'));
+        table.string('email').unique().notNullable();
+        table.string('password').notNullable();
+        table.boolean('is_admin').defaultTo(false);
+        table.timestamp('created_at').defaultTo(transaction.fn.now());
+        table.timestamp('updated_at').defaultTo(transaction.fn.now());
+      });
+    });
+  }
+
+  @MigrationLogger
+  static async down(knex: Knex): Promise<void> {
+    try {
+      await knex.transaction(async transaction => {
+        const isTableExists = await transaction.schema.hasTable('users');
+        if (!isTableExists) {
+          migrationsLogger.warn('Table users does not exist');
+          return;
+        }
+
+        await transaction.schema.dropTable('users');
+        migrationsLogger.info('Table users deleted');
+        await transaction.commit();
+      });
+    } catch (error) {
+      migrationsLogger.error('Table users not deleted');
+      throw error;
+    }
+  }
 }
 
-export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('users');
-}
+export const { up, down } = Migration;
