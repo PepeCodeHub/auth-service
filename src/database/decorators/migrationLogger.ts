ecf4481412293031f6ae8a5062f03f9eec10f080
Ihor Cherniavskyi <chernyavskiy2019ios@gmail.com>
2024-12-12 02:16:24 +0200

    Remove ESLint disable comments for 'any' type in migration decorators

--- src/database/decorators/migrationLogger.ts
+++ src/database/decorators/migrationLogger.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import winston from 'winston';
 import path from 'path';
 
--- src/database/decorators/skipRollback.ts
+++ src/database/decorators/skipRollback.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { migrationsLogger } from './migrationLogger';
 
 export const SkipRollback = (
