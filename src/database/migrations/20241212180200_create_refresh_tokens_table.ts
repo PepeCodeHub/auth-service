9193ab1e0341782b03e40561b563bd5ffb74ffe9
Ihor Cherniavskyi <chernyavskiy2019ios@gmail.com>
2024-12-12 20:04:30 +0200

    Add migration for refresh_tokens table with up and down methods

---
+++ src/database/migrations/20241212180200_create_refresh_tokens_table.ts
@@ -0,0 +1,55 @@
+import { Knex } from 'knex';
+import { MigrationLogger, migrationsLogger } from '../decorators';
+
+export class Migration {
+  @MigrationLogger
+  static async up(knex: Knex): Promise<void> {
+    await knex.transaction(async transaction => {
+      try {
+        const isTableExists = await transaction.schema.hasTable('refresh_tokens');
+        if (isTableExists) {
+          migrationsLogger.warn('Table refreshtokens already exists');
+          return;
+        }
+
+        await transaction.schema.createTable('refresh_tokens', table => {
+          table
+            .uuid('id')
+            .primary()
+            .defaultTo(transaction.raw('gen_random_uuid()'));
+          table.uuid('user_id').notNullable();
+          table.string('token').notNullable();
+          table.timestamp('created_at').defaultTo(transaction.fn.now());
+          table.timestamp('updated_at').defaultTo(transaction.fn.now());
+        });
+
+      } catch (error) {
+        migrationsLogger.error(error);
+        throw error;
+      }
+    });
+  }
+
+  @MigrationLogger
+  static async down(knex: Knex): Promise<void> {
+    await knex.transaction(async transaction => {
+      try {
+        const isTableExists = await transaction.schema.hasTable('refresh_tokens');
+        if (!isTableExists) {
+          migrationsLogger.warn('Table refreshtokens does not exist');
+          return;
+        }
+
+        await transaction.schema.dropTable('refresh_tokens');
+        await transaction.commit();
+        migrationsLogger.info('Table refresh_tokens deleted');
+
+      } catch (error) {
+        migrationsLogger.error(error);
+        throw error;
+      }
+    });
+  }
+}
+
+export const { up, down } = Migration;
