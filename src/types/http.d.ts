1c5837cfbec37fc309201c25d025d92b2fa5f776
Ihor Cherniavskyi <chernyavskiy2019ios@gmail.com>
2025-05-03 23:35:36 +0300

    Add HttpMethod enum and user-related types for authentication (#5)

--- src/types/http.d.ts
+++ src/types/http.d.ts
@@ -1,12 +1,3 @@
-export enum HttpMethod {
-  GET = 'GET',
-  POST = 'POST',
-  PUT = 'PUT',
-  PATCH = 'PATCH',
-  DELETE = 'DELETE',
-  HEAD = 'HEAD',
-}
-
 export interface AuthRequest {
   action: HttpMethod;
   path: string;
